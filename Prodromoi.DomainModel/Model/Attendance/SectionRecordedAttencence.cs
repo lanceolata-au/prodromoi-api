a9863b759b50ce8b719c904f9b95a2076bb0329e
Owen Holloway <owen.holloway@lanceolata.com.au>
2023-05-12 20:45:38 +1000

    cleaning up warnings

--- Prodromoi.DomainModel/Model/Attendance/SectionRecordedAttencence.cs
+++ Prodromoi.DomainModel/Model/Attendance/SectionRecordedAttencence.cs
@@ -7,9 +7,9 @@ namespace Prodromoi.DomainModel.Model.Attendance;
 public class SectionRecordedAttendance : AuditEntity
 {
     public int RecordingAdultId { get; private set; } = 0;
-    public virtual Member RecordingAdult { get; private set; }
+    public virtual Member RecordingAdult { get; private set; } = null!;
     public int FormationSectionId { get; private set; } = 0;
-    public virtual FormationSection FormationSection { get; private set; }
+    public virtual FormationSection FormationSection { get; private set; } = null!;
     public DateTime Recorded { get; private set; } = DateTime.MinValue;
 
     public static SectionRecordedAttendance Create(Member member, int formationSectionId)
